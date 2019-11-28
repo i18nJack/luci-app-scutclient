a8f9b8eaab28a3c7521a00cc85019dd22a8f4615
pangan <pangan@2013@gmail.com>
2019-11-28 19:21:45 +0800

    optimize

--- luasrc/controller/scutclient.lua
+++ luasrc/controller/scutclient.lua
@@ -13,54 +13,31 @@ function index()
 	end
 	local uci = require "luci.model.uci".cursor()
 	local mainorder = uci:get_first("scutclient", "luci", "mainorder", 10)
-	if not uci:get_first("scutclient", "luci", "configured", false) then
-		entry({"admin", "network", "scutclient"},
-			alias("admin", "network", "scutclient", "settings"),
-			"校园网",
-			mainorder
-		)
+    
+    entry({"admin", "network", "scutclient"},
+        alias("admin", "network", "scutclient", "status"), "华工无线", mainorder
+    )
 
-		entry({"admin", "scutclient", "settings"},
-			cbi("scutclient/scutclient"),
-			"设置",
-			10
-		).leaf = true
+    entry({"admin", "network", "scutclient", "status"},
+        call("action_status"), "状态", 10
+    ).leaf = true
 
-		entry({"admin", "scutclient", "status"},
-			call("action_status"),
-			"状态",
-			20
-		).leaf = true
-	else
-		entry({"admin", "network", "scutclient"},
-			alias("admin", "network", "scutclient", "status"),
-			"校园网",
-			mainorder
-		)
-
-		entry({"admin", "scutclient", "status"},
-			call("action_status"),
-			"状态",
-			10
-		).leaf = true
-
-		entry({"admin", "scutclient", "settings"},
-			cbi("scutclient/scutclient"),
-			"设置",
-			20
-		).leaf = true
-	end
-	entry({"admin", "scutclient", "logs"}, template("scutclient/logs"), "日志", 30).leaf = true
-	entry({"admin", "scutclient", "get_log"}, call("get_log"))
-	entry({"admin", "scutclient", "netstat"}, call("get_netstat"))
-	entry({"admin", "scutclient", "scutclient-log.tar"}, call("get_dbgtar"))
+    entry({"admin", "network", "scutclient", "settings"},
+        cbi("scutclient/scutclient"), "设置", 20
+    ).leaf = true
+    
+	entry({"admin", "network", "scutclient", "logs"}, 
+        template("scutclient/logs"), "日志", 30
+    ).leaf = true
+    
+	entry({"admin", "network", "scutclient", "get_log"}, call("get_log"))
+	entry({"admin", "network", "scutclient", "netstat"}, call("get_netstat"))
 end
 
 
 function get_log()
-	local send_log_lines = 75
 	if fs.access(log_file) then
-		client_log = sys.exec("tail -n "..send_log_lines.." " .. log_file)
+		client_log = sys.exec("tail -n 60 " .. log_file)
 	else
 		client_log = "Unable to access the log file!"
 	end
@@ -79,11 +56,6 @@ function action_status()
 		luci.sys.call("/etc/init.d/scutclient stop > /dev/null")
 		luci.sys.call("/etc/init.d/scutclient start > /dev/null")
 	end
-	if luci.http.formvalue("move_tag") == "1" then
-		luci.sys.call("uci set scutclient.@luci[-1].mainorder=90")
-		luci.sys.call("uci commit")
-		luci.sys.call("rm -rf /tmp/luci-*cache")
-	end
 end
 
 function get_netstat()
@@ -98,37 +70,5 @@ function get_netstat()
 	end
 	http.prepare_content("application/json")
 	http.write_json(nstat)
-	http.close()
-end
-
-function get_dbgtar()
-
-	local tar_dir = "/tmp/scutclient-log"
-	local tar_files = {
-		"/etc/config/wireless",
-		"/etc/config/network",
-		"/etc/config/system",
-		"/etc/config/scutclient",
-		"/etc/openwrt_release",
-		"/etc/crontabs/root",
-		"/etc/config/dhcp",
-		"/tmp/dhcp.leases",
-		"/etc/rc.local",
-	}
-
-	fs.mkdirr(tar_dir)
-	table.foreach(tar_files, function(i, v)
-			luci.sys.call("cp " .. v .. " " .. tar_dir)
-	end)
-
-	if fs.access(log_file_backup) then
-		luci.sys.call("cat " .. log_file_backup .. " >> " .. tar_dir .. "/scutclient.log")
-	end
-	if fs.access(log_file) then
-		luci.sys.call("cat " .. log_file .. " >> " .. tar_dir .. "/scutclient.log")
-	end
-	http.prepare_content("application/octet-stream")
-	http.write(sys.exec("tar -C " .. tar_dir .. " -cf - ."))
-	luci.sys.call("rm -rf " .. tar_dir)
 	http.close()
 end
